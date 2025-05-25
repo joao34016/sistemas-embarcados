2fe26f2fbf96ac8c5f41b6a591731d6107864d4a
joao34016 <cardosomaycon298@gmail.com>
2025-05-25 18:56:50 -0300

    Add files via upload
    exercicio que pisca os leds quando for digitado algo no serial

---
+++ exercicio4.ino
@@ -0,0 +1,33 @@
+#define pin_led1 8   
+#define pin_led2 9    
+#define pin_led3 10   
+#define pin_led4 11   
+
+
+void setup() {
+  Serial.begin(9600);          
+  pinMode (pin_led1, OUTPUT);  
+  pinMode (pin_led2, OUTPUT);  
+  pinMode (pin_led3, OUTPUT);  
+  pinMode (pin_led4, OUTPUT);  
+
+}
+
+void loop() {
+  if (Serial.available() == 1){
+  	Serial.read();
+  
+ Serial.println("liga LED");    
+ digitalWrite (pin_led1, HIGH);  
+ digitalWrite (pin_led2, HIGH);  
+ digitalWrite (pin_led3, HIGH);  
+ digitalWrite (pin_led4, HIGH);  
+ delay (500);                    
+ Serial.println("desliga LED");  
+ digitalWrite (pin_led1, LOW);   
+ digitalWrite (pin_led2, LOW);   
+ digitalWrite (pin_led3, LOW);   
+ digitalWrite (pin_led4, LOW);   
+ delay (500);                    
+}
+}
