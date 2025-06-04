1c2954462cd126905147b4af9b6b852fbce409a0
joao34016 <cardosomaycon298@gmail.com>
2025-06-04 15:22:11 -0300

    Add files via upload
    Serial Hello World

---
+++ Aula13_ex1.ino
@@ -0,0 +1,9 @@
+// eu faltei na aula nesse dia professor marcos, então acredito que o código seja apenas isso.
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Hello World");
+  delay(1000);
+}
